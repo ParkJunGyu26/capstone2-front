8e62c267954e6802f2f20129da13383e2874230e
박준규 <junju404@naver.com>
2024-10-28 15:04:44 +0900

    fix: update API_URL and workflow

--- src/components/TeacherDashboard.js
+++ src/components/TeacherDashboard.js
@@ -10,10 +10,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const API_URL =
-  process.env.REACT_APP_API_URL || process.env.NODE_ENV === "development"
-    ? "http://localhost:8080"
-    : "https://13.125.153.61:8081";
+const API_URL = process.env.REACT_APP_API_URL;
 
 const TeacherDashboard = () => {
   const [units, setUnits] = useState([]);
